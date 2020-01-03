ae08d109f1f87149a1b497f348007f19af3b3d60
bqanderson <bqanderson@me.com>
2020-01-03 14:49:12 -0600

    Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Header from './components/Header'
 
-function App() {
-  return <div>Website build using Wordpress API and React.js</div>
-}
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+      <section className='section container content'>
+        <h1>Website build using Wordpress API and React.js</h1>
+      </section>
+    </div>
+  </Router>
+)
 
 export default App
